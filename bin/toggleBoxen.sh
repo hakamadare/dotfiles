488ce70f56a6e93f0f4807d40f68219ee08d8554
Steve Huff <shuff@vecna.org>
2014-05-03 10:53:26 -0400

    more bash hacking

--- bin/toggleBoxen.sh
+++ bin/toggleBoxen.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 #
 # toggle Boxen between multiple users
-if [[ -d $BOXEN_HOME ]]; then
+source /opt/boxen/env.sh
+
+# who is the real user?
+if [[ -z $REAL_USER ]]; then
+  exit 1
+elif [[ -d $BOXEN_HOME ]]; then
   # fix ownership
-  chown -R $USER $BOXEN_HOME 2>/dev/null
+  chown -R $REAL_USER $BOXEN_HOME 2>/dev/null
 
-  BOXEN_CONFIG_DEFAULTS="${BOXEN_CONFIG_DIR}/boxen/default.json"
-
-  if [[ -f $BOXEN_CONFIG_DEFAULTS ]]; then
+  BOXEN_CONFIG_DEFAULTS="${BOXEN_CONFIG_DIR}/boxen/defaults.json"
+  if [[ -w $BOXEN_CONFIG_DEFAULTS ]]; then
     # fix config
     cat >$BOXEN_CONFIG_DEFAULTS 2>/dev/null <<DEFAULTS
 {
   "s3bucket": "boxen-downloads",
   "s3host": "s3.amazonaws.com",
   "repotemplate": "https://github.com/%s",
-  "user": "$USER",
-  "srcdir": "${HOME}/src",
+  "user": "$REAL_USER",
+  "srcdir": "/Users/${REAL_USER}/src",
   "ghurl": "https://github.com",
   "email": "shuff@vecna.org",
   "fde": false,
@@ -23,7 +27,7 @@ if [[ -d $BOXEN_HOME ]]; then
   "login": "hakamadare",
   "name": "Steve Huff",
   "puppetdir": "/tmp/boxen/puppet",
-  "repodir": "${HOME}/git/our-boxen",
+  "repodir": "/Users/${REAL_USER}/git/our-boxen",
   "reponame": "hakamadare/our-boxen"
 }
 DEFAULTS
