92cde1adc9b13f02ecae37ca8c3cb9531c96c993
Steve Huff <shuff@vecna.org>
2014-05-09 09:36:39 -0400

    use a SUDO_ env var

--- bin/toggleBoxen.sh
+++ bin/toggleBoxen.sh
@@ -4,12 +4,12 @@
 source /opt/boxen/env.sh
 
 # who is the real user?
-if [[ -z $REAL_USER ]]; then
+if [[ -z $SUDO_REAL_USER ]]; then
   exit 1
 elif [[ -d $BOXEN_HOME ]]; then
   # fix ownership
-  chown -R $REAL_USER $BOXEN_HOME 2>/dev/null
-  chown -R $REAL_USER /Library/Caches/Homebrew 2>/dev/null
+  chown -R $SUDO_REAL_USER $BOXEN_HOME 2>/dev/null
+  chown -R $SUDO_REAL_USER /Library/Caches/Homebrew 2>/dev/null
 
   BOXEN_CONFIG_DEFAULTS="${BOXEN_CONFIG_DIR}/boxen/defaults.json"
   if [[ -w $BOXEN_CONFIG_DEFAULTS ]]; then
@@ -19,8 +19,8 @@ elif [[ -d $BOXEN_HOME ]]; then
   "s3bucket": "boxen-downloads",
   "s3host": "s3.amazonaws.com",
   "repotemplate": "https://github.com/%s",
-  "user": "$REAL_USER",
-  "srcdir": "/Users/${REAL_USER}/src",
+  "user": "$SUDO_REAL_USER",
+  "srcdir": "/Users/${SUDO_REAL_USER}/src",
   "ghurl": "https://github.com",
   "email": "shuff@vecna.org",
   "fde": false,
@@ -28,7 +28,7 @@ elif [[ -d $BOXEN_HOME ]]; then
   "login": "hakamadare",
   "name": "Steve Huff",
   "puppetdir": "/tmp/boxen/puppet",
-  "repodir": "/Users/${REAL_USER}/git/our-boxen",
+  "repodir": "/Users/${SUDO_REAL_USER}/git/our-boxen",
   "reponame": "hakamadare/our-boxen"
 }
 DEFAULTS
