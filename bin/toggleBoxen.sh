84aeed72f855546659736c4163b752a8361c6d59
Steve Huff <shuff@vecna.org>
2015-01-26 11:22:16 -0500

    chown /opt/rubies

--- bin/toggleBoxen.sh
+++ bin/toggleBoxen.sh
@@ -10,6 +10,7 @@ elif [[ -d $BOXEN_HOME ]]; then
   # fix ownership
   chown -R $SUDO_REAL_USER $BOXEN_HOME 2>/dev/null
   chown -R $SUDO_REAL_USER /Library/Caches/Homebrew 2>/dev/null
+  chown -R $SUDO_REAL_USER /opt/rubies 2>/dev/null
 
   BOXEN_CONFIG_DEFAULTS="${BOXEN_CONFIG_DIR}/boxen/defaults.json"
   if [[ -w $BOXEN_CONFIG_DEFAULTS ]]; then
